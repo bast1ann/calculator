7a129dc4d432baa75c8f9c347a4abffe9a6a80bf
bast1ann <163662587+bast1ann@users.noreply.github.com>
2024-04-26 13:58:09 -0400

    Add javascript file and basic functions

---
+++ script.js
@@ -0,0 +1,33 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(number1, number2, operator) {
+  switch (operator) {
+    case "+":
+      return add(number1, number2);
+    case "-":
+      return subtract(number1, number2);
+    case "*":
+      return multiply(number1, number2);
+    case "/":
+      return divide(number1, number2);
+  }
+}
+
