0fbc1857e814cb03ca764fe22f5aeb4d0311166c
bast1ann <163662587+bast1ann@users.noreply.github.com>
2024-04-28 03:46:33 -0400

    Add functionality that displays numbers when clicking buttons

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll("button.number");
+
+numberButtons.forEach( (button) => button.addEventListener("click", writeDisplay) );
+
+function writeDisplay() {
+  display.textContent == "0" ?
+  display.textContent = this.textContent :
+  display.textContent += this.textContent;
+}
+
 function add(a, b) {
   return a + b;
 }
